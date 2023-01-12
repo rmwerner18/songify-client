c24c0b1f5f75bad80ec19309396a829602375a09
humorous-storage-7521 <rmwerner@umich.edu>
2023-01-11 21:14:28 -0600

    focus search

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,14 +1,19 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const SearchBar = props => {
     let [active, setActive] = useState(false)
+    const searchInput = useRef(null)
+
+    useEffect(() => {
+        searchInput.current.focus()
+    }, [active])
 
     return (
         <div className={active ? 'search active' : 'search'}>
-            <input className='search-bar' type="text" onChange={props.handleSearch} value={props.searchInput} />
+            <input className='search-bar' type="search" ref={searchInput} onChange={props.handleSearch} value={props.searchInput} />
             <FontAwesomeIcon className='search-icon' icon={solid('magnifying-glass')} onClick={() => setActive(!active)}/>
         </div>
     )
