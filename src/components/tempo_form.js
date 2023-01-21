4ca714b58b1a37481b1778ef671db75cdabbb711
humorous-storage-7521 <ryanwerner50@gmail.com>
2023-01-21 12:20:48 -0600

    fix tempo form

--- src/components/tempo_form.js
+++ src/components/tempo_form.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { changeSongAttribute } from '../actions/change_song_attribute';
 
-const TempoForm = (props) => {
+const TempoForm = () => {
     const dispatch = useDispatch()
     const bpm = useSelector(state => state.currentSong.bpm)
-
+    console.log(bpm)
     return (
         <div className='tempo-form'>
-            <p className="bpm-meter">bpm: {bpm}</p>
-            <input type="range" min="30" max='300' value={props.bpm} onChange={(e) => dispatch(changeSongAttribute({ bpm: e.target.value }))}/>
+            <p className='bpm-meter'>bpm: {bpm}</p>
+            <input type='range' min='30' max='300' value={bpm} onChange={(e) => dispatch(changeSongAttribute({ bpm: e.target.value }))}/>
         </div>
     )
 }
